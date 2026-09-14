f68472aa614bbbe0dd67297dd8ca4fcb215cbe66
StevenRP97 <lstevenrpl97@gmail.com>
2026-09-14 09:59:08 -0600

    Initial commit

---
+++ 202603/Semana 2/Prog2_Semana2_Lunes/src/main/java/uam/semana2/SolucionMatricula.java
@@ -0,0 +1,16 @@
+package uam.semana2;
+// Importar librería de lectura de datos
+import java.util.Scanner;
+
+public class SolucionMatricula {
+    public static void main(String[] args) {
+        // 1. Inicialización de variables
+        double precioMateria = 85000,0;
+        boolean esPrimerIngreso = true;
+        int cantidadMaterias;
+        
+        // 2. Declaración de lector
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
---
+++ 202603/clase2-1.java
@@ -0,0 +1,7 @@
+public class SolucionMatricula{
+	public.static.void main (String[]args){
+		// 1. Declaración e inicialización de variables
+		double costoMateria = 85000.0;
+		boolean esPrimerIngreso = true;
+	}
+}
