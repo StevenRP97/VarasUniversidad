e91e1f81a20ee4bcd659369855e0c30626d00f1f
StevenRP97 <lstevenrpl97@gmail.com>
2026-09-21 09:30:18 -0600

    Semana 3 Lunes

---
+++ 202603/Semana 3/Prog1_Semana3_Lunes/src/uam/semana3/EjercicioDobleNotas_Plantilla.java
@@ -0,0 +1,66 @@
+package uam.semana3; 
+
+// Importación de la herramienta para leer flujos de texto del teclado 
+import java.io.PrintStream; 
+import java.io.UnsupportedEncodingException; 
+import java.util.Scanner; 
+
+/* 
+ * Fecha: 1/5/26 
+ * Semana: 3 
+ * Ejercicio: 2 
+ * Autor: Rutbelt Rodriguez
+ */ 
+
+public class EjercicioDobleNotas_Plantilla {
+    public static void main(String[] args) { 
+        // FORZAR SALIDA EN CONSOLA (Para las tildes y el colón) 
+        // Intentamos usar UTF-8 nativo en el flujo de impresión, si falla usamos Windows-1252 
+
+        try { 
+            System.setOut(new PrintStream(System.out, true, "UTF-8")); 
+
+        } catch (UnsupportedEncodingException e) { 
+            try { 
+                System.setOut(new PrintStream(System.out, true, "Windows-1252")); 
+
+            } catch (UnsupportedEncodingException ex) { 
+                // Si ambos fallan, el sistema continúa por defecto 
+            } 
+        } 
+        // Inicialización del objeto para capturar datos de consola 
+        Scanner teclado = new Scanner(System.in, "UTF-8"); 
+
+        // Variable para almacenar la calificación del estudiante 
+        //?? 
+        System.out.println("=== SISTEMA DE ACTAS ACADÉMICAS UAM ==="); 
+
+        // Captura de la calificación obtenida por el alumno 
+        //?? 
+
+        // EVALUACIÓN SELECTIVA DOBLE (Caminos mutuamente excluyentes) 
+        // Primer camino: Comprobamos si cumple el criterio de aprobación 
+        //?? 
+
+            // Este bloque se ejecuta únicamente si la nota es igual o mayor a 70 
+
+            //?? 
+
+            // El bloque else se ejecuta obligatoriamente si la condición del 'if' da falso (nota menor a 70) 
+
+            //?? 
+
+        } 
+
+ 
+
+        //System.out.println("======================================="); 
+
+         
+
+        // Finalización del flujo de entrada 
+
+        //?? 
+
+    } 
+
